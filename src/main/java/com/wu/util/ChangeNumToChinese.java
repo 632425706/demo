c59e077f1cda1179eb6ef127aca1924cb0c52315
wuzhentao <734757460@qq.com>
2018-09-11 23:28:13 +0800

    当日新增人数

---
+++ src/main/java/com/wu/util/ChangeNumToChinese.java
@@ -0,0 +1,29 @@
+package com.wu.util;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class ChangeNumToChinese {
+    public static String toChinese(String str) {
+        String[] s1 = { "零", "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+        String[] s2 = { "十", "百", "千", "万", "十", "百", "千", "亿", "十", "百", "千" };
+        String result = "";
+        int n = str.length();
+        for (int i = 0; i < n; i++) {
+            int num = str.charAt(i) - '0';
+            if (i != n - 1 && num != 0) {
+                result += s1[num] ;
+//                result += s1[num] + s2[n - 2 - i];
+            } else {
+                result += s1[num];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Calendar a=Calendar.getInstance();
+        int year=a.get(Calendar.YEAR);
+        System.out.println(toChinese(year+""));
+    }
+}
